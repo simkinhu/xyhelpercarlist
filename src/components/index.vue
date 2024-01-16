888bb1c009a30ce15bd75eec38c5d34e0642bc9a
Simkin <mr.xiaosi@qq.com>
2024-01-17 00:54:58 +0800

    更新分页参数，未完成

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,47 +1,41 @@
 <template>
-  <n-global-style />
+  <n-global-style/>
   <n-grid x-gap="12" :cols="1">
     <n-gi>
       <div class="light-green" v-html="notice">
       </div>
     </n-gi>
   </n-grid>
-  <n-divider>001</n-divider>
+  <n-divider></n-divider>
   <n-grid x-gap="12" y-gap="12" cols="2 400:4 600:6">
     <n-grid-item class="cardclss" v-for="item in itemsfree" :key="item.carID">
-      <n-card :title="item.carID" @click="redirectTo(item.carID)">
-        <img class="plusicon"
-             :src="'https://img.closeai.biz/endpoint?url=https%3A%2F%2Ffree-gpt.club%2Fendpoint%3Fcarid%3D' + item.carID">
-      </n-card>
-    </n-grid-item>
-  </n-grid>
-
-  <n-divider>002</n-divider>
-  <n-grid x-gap="12" y-gap="12" cols="2 400:4 600:6">
-    <n-grid-item class="cardclss" v-for="item in itemsplus" :key="item.carID">
       <n-card :title="item.carID" @click="redirectTo(item.carID)">
         <img class="plusicon" :src="'https://img.closeai.biz/endpoint?url=' + item.iconurl">
       </n-card>
     </n-grid-item>
   </n-grid>
+  <n-divider></n-divider>
+  <n-space>
+    <n-text class="total">共：{{ total }} 条数据</n-text>
+    <n-button size="small" type="info">
+      Info
+    </n-button>
+
+  </n-space>
 
 </template>
 
 <script lang="ts">
 import axios from 'axios';
+
 export default {
   data() {
     return {
       itemsfree: [],
       itemsplus: [],
-      notice:"",
-      chatimages: [{
-        'title': 'ChatGPT-001',
-        'url': 'https://chat.freegpts.org/login'
-      }, {
-        'title': 'ChatGPT-002',
-        'url': 'https://chat.freegpts.org/login'
-      },]
+      notice: "",
+      total:0,
+      page: 1
     };
   },
   mounted() {
@@ -49,29 +43,30 @@ export default {
   },
   methods: {
     fetchData() {
-      axios.post('/carpage', {})
+      axios.post('https://free-gpt.club/carpage', {
+        page: this.page,
+        size: 6
+      })
           .then(response => {
             console.log(response);
             this.notice = response.data.notice;
+            this.total = response.data.data.pagination.total;
             let baseUrl = window.location.href;
             const allItems = response.data.data.list.map(item => {
               let iconUrl = `${baseUrl}endpoint?carid=${item.carID}`;
               iconUrl = encodeURIComponent(iconUrl);
-              return { ...item, iconurl: iconUrl };
+              return {...item, iconurl: iconUrl};
             });
             console.log(allItems);
-            this.itemsfree = allItems.filter(item => item.isPlus === 0);
-            this.itemsplus = allItems.filter(item => item.isPlus === 1);
+            this.itemsfree = allItems;
           })
           .catch(error => {
             console.error('请求错误:', error);
           });
     },
     redirectTo(carID) {
-      const url = `https://free-gpt.club/auth/login?carid=${carID}`;
-      window.location.href = url;
-    },
-    redirectToimages(url) {
+      let baseUrl = window.location.href;
+      const url = `${baseUrl}auth/login?carid=${carID}`;
       window.location.href = url;
     }
   }
