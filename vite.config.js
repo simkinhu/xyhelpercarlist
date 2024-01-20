65ee17b8829cbed9d47459715c6a8226efb8d177
Simkin <mr.xiaosi@qq.com>
2024-01-20 17:31:18 +0800

    bugXIUXIUXIU！

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     devServer: {
         proxy: {
             "/carpage": {
-                target: "https://free-gpt.club",
+                target: "http://127.0.0.1:8300",
                 changeOrigin: true,
                 ws: true,
                 autoRewrite: true,
