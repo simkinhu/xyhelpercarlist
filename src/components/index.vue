5956b9f19f05b68f358605509902219838e14ed6
simkinhu <723179227@qq.com>
2024-05-01 22:30:07 +0800

    kaifaban

--- src/components/index.vue
+++ src/components/index.vue
@@ -11,25 +11,40 @@
         </template>
       </n-switch>
       <div v-html="notice"></div>
+      <!--<div v-html="useravatar"></div>-->
     </n-card>
 
-    <n-grid x-gap="10" y-gap="10" cols="2 s:3 m:4 l:5 xl:5 2xl:6" responsive="screen">
+    <n-grid x-gap="10" y-gap="10" cols="1 s:2 m:2 l:2 xl:2 2xl:2" responsive="screen">
+
+      <n-grid-item class="leftbox">
+        <div class="avaimg" v-html="useravatar"></div>
+        <div class="userinfo">
+          <div class="username">UserToken：723179227</div>
+          <div class="username">UserToken：723179227</div>
+          <div class="username">UserToken：723179227</div>
+        </div>
+      </n-grid-item>
+
+      <n-grid-item class="leftbox">
+        <div class="avaimg" v-html="useravatar"></div>
+        <div class="info">UserToken：723179227</div>
+      </n-grid-item>
+
+    </n-grid>
+
+
+    <n-grid x-gap="10" y-gap="10" cols="1 s:3 m:4 l:5 xl:5 2xl:6" responsive="screen">
+
       <n-grid-item class="cardclss" v-for="item in itemslist" :key="item.carID">
-
         <n-card size="small" content-style="boxclass" :content-class="'boxclass ' + (item.isPlus ? 'pluscolor' : 'nopluscolor')" @click="redirectTo(item.carID)">
-
-          <div class="type" :style="{ background: item.labelColor }">{{ item.label != ""? item.label: "NO" }}</div>
-
+          <div class="type" :style="{ background: item.endpoint.labelColor }">{{ item.endpoint.label != "" ? item.endpoint.label : "NO" }}</div>
           <div class="cartitle">
             <n-text class="title">{{ item.carID }}</n-text>
           </div>
-
-          <div class="message-with-dot">实时状态：{{ item.message }}</div>
-
+          <div class="message-with-dot">实时状态：{{ item.endpoint.message }}</div>
           <div :style="{ width: '100%' }">
-            <a-progress :show-text="false" :steps="4" :color=item.color :percent="item.bai" :track-color="(item.labelColor)" stroke-width='30'/>
+            <a-progress :show-text="false" :steps="4" :color=item.endpoint.color :percent="item.statusinfo.percent" :track-color="(item.endpoint.labelColor)" stroke-width='30'/>
           </div>
-
         </n-card>
 
       </n-grid-item>
@@ -45,6 +60,22 @@
   float: right;
 }
 
+.n-grid{
+  margin-bottom: 15px;
+}
+.leftbox{
+  padding: 20px;
+  border-radius: 8px;
+  background-color: #F1F4F7FF;
+}
+.avaimg{
+  margin-right: 20px;
+  display: inline-block;
+  width: 60px;
+}
+.userinfo{
+  display: inline-block;
+}
 .n-gradient-text {
   margin-top: 10px;
 }
@@ -128,43 +159,24 @@
 
 
 <script lang="ts">
+import multiavatar from '@multiavatar/multiavatar/esm';
 import axios from 'axios';
-import {darkTheme} from 'naive-ui'
-import type {GlobalTheme} from 'naive-ui'
-import {Sparkles, SunnySharp} from '@vicons/ionicons5'
-
+import {ref, defineComponent, onMounted, watch} from 'vue';
+import {darkTheme} from 'naive-ui';
+import type {GlobalTheme} from 'naive-ui';
 
 export default defineComponent({
   data() {
     return {
       itemslist: [],
-      itemsplus: [],
+      useravatar: "",
+      userinfo: [],
       notice: "",
       total: 0,
       page: 1,
       isLoading: false,
       hasMoreData: true
     };
-  },
-  computed: {
-    AccessibilitySharp() {
-      return AccessibilitySharp
-    },
-    GameController() {
-      return GameController
-    },
-    LogoNodejs() {
-      return LogoNodejs
-    },
-    Airplane() {
-      return Airplane
-    },
-    Sparkles() {
-      return Sparkles
-    },
-    SunnySharp() {
-      return SunnySharp
-    },
   },
   setup() {
     const theme = ref<GlobalTheme | null>(null);
@@ -182,122 +194,83 @@ export default defineComponent({
     }
     watch(active, (newValue) => {
       theme.value = newValue ? darkTheme : null;
-      localStorage.setItem('theme', theme.value ? 'darkTheme' : 'lightTheme');
+      localStorage.setItem('theme', newValue ? 'darkTheme' : 'lightTheme');
     });
 
-    // 页面加载时恢复主题
     onMounted(() => {
       restoreTheme();
       document.body.classList.remove('loading');
     });
-    return {theme, active, Sparkles, SunnySharp};
-    return {
-      theme,
-      active,
-      Sparkles,
-      SunnySharp
-    }
+
+    return {theme, active};
   },
 
   created() {
     this.fetchData();
+    this.registerUser();
   },
 
   mounted() {
     window.addEventListener('scroll', this.handleScroll);
   },
+
   methods: {
-    fetchData: async function () {
+    registerUser() {
+      const url = 'http://47.76.135.99:8310/api/userinfo';
+      axios.post(url, {
+        "usertoken": 723179227
+      }).then(response => {
+        // this.userinfo = response.data[0].data
+        console.log(response.data.data[0].userToken)
+        this.useravatar = multiavatar(response.data.data[0].userToken)
+        console.log(multiavatar(response.data.data[0].userToken));
+
+
+      })
+          .catch(error => {
+            // 处理错误情况
+            this.responseMessage = 'Registration failed!';
+            console.error('Error:', error);
+          });
+    },
+    fetchData() {
       if (!this.hasMoreData || this.isLoading) return;
-      this.isLoading = true; // 开始加载，设置为true
+      this.isLoading = true;
       axios.post('/carpage', {
         page: this.page,
-        size: 50
-      })
-          .then(response => {
-            if (response.data.data.list === null) {
-              this.hasMoreData = false;
-              this.isLoading = false;
-            } else {
-              this.notice = response.data.notice;
-              let baseUrl = window.location.origin;
-              let promises = response.data.data.list.map(item => {
-                let carname = encodeURIComponent(`${item.carID}`);
-                let endpointUrl = `${baseUrl}/endpoint?carid=${carname}`;
-                let statusUrl = `${baseUrl}/status?carid=${carname}`;
-
-                // 并行发起 endpoint 和 status 请求
-                let endpointPromise = fetch(endpointUrl)
-                    .then(response => response.json())
-                    .catch(error => {
-                      console.error('Error fetching endpoint data:', error);
-                      return {};
-                    });
-
-                let statusPromise = fetch(statusUrl)
-                    .then(response => response.json())
-                    .catch(error => {
-                      console.error('Error fetching status data:', error);
-                      return {};
-                    });
-                return Promise.all([endpointPromise, statusPromise]).then(([endpointData, statusData]) => {
-                  function replaceStopRunning(text) {
-                    return text.replace("PLUS停运｜", "")
-                        .replace("TEAM停运｜", "")
-                        .replace("停运｜", "")
-                        .replace("|", "-")
-                        .replace("green", "#f9bd5f")
-                        .replace("yellow", "#f9bd5f")
-                        .replace("red", "#f65e5d")
-                        .replace("PLUS", "Plus")
-                        .replace("blue", "#24d4ae")
-                        .replace("purple", "#a07be6");
-                  }
-
-                  for (let key in endpointData) {
-                    if (typeof endpointData[key] === 'string') {
-                      endpointData[key] = replaceStopRunning(endpointData[key]);
-                    }
-                  }
-                  let loadbai = item.isPlus == true ? (statusData.count / 80) * 1 : (statusData.count / 500) * 1;
-                  let bai = loadbai > 0.2 ? loadbai : 0;
-                  return {...item, ...endpointData, ...statusData, bai: bai.toFixed(5)};
-                });
-              });
-
-              Promise.all(promises).then(newItems => {
-                const existingIds = new Set(this.itemslist.map(item => item.carID));
-                const filteredItems = newItems.filter(item => !existingIds.has(item.carID));
-                this.itemslist = [...this.itemslist, ...filteredItems];
-                this.page += 1;
-              }).catch(error => {
-                console.error('请求错误:', error);
-              });
-            }
-          })
-          .catch(error => {
-            console.error('请求错误:', error);
-          })
-          .finally(() => {
-            this.isLoading = false;
-          })
+        size: 72
+      }).then(response => {
+        if (!response.data || !response.data.data || !response.data.data.list) {
+          this.hasMoreData = false;
+          return;
+        }
+        const {list} = response.data.data;
+        if (list.length === 0) {
+          this.hasMoreData = false;
+        } else {
+          this.notice = response.data.data.notice;
+          this.itemslist = [...this.itemslist, ...list];
+          this.page++;
+        }
+      }).catch(error => {
+        console.error('请求错误:', error);
+      }).finally(() => {
+        this.isLoading = false;
+      });
     },
     handleScroll() {
       const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
       const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
       const clientHeight = document.documentElement.clientHeight;
       const nearBottomOfPage = scrollTop + clientHeight >= scrollHeight - 100;
-      if (nearBottomOfPage && !this.isLoading && this.hasMoreData) {
+      if (nearBottomOfPage && this.hasMoreData && !this.isLoading) {
         this.fetchData();
       }
-    },
-    redirectTo(carID) {
-      window.location.href = `${window.location.origin}/auth/login?carid=${encodeURI(carID)}`;
     },
     beforeDestroy() {
       window.removeEventListener('scroll', this.handleScroll);
     }
-
   }
-});
+})
+;
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,62 @@
-import {fileURLToPath, URL} from 'node:url'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
 
-
-export default defineConfig({
-    transpileDependencies: true,
-    //打包后不生成.map文件
-    productionSourceMap: false,
-    base: "/list",
-    devServer: {
-        proxy: {
-            "/carpage": {
-                target: "http://127.0.0.1:8300",
-                changeOrigin: true,
-                ws: true,
-                autoRewrite: true,
-                cookieDomainRewrite: true,
+    return defineConfig({
+        base: env.VITE_BASE_URL || '/list',
+        server: {
+            proxy: {
+                '/carpage': {
+                    target: 'http://47.76.135.99:8310',
+                    changeOrigin: true,
+                    ws: true,
+                    rewrite: path => path.replace(/^\/carpage/, '/api/list')
+                },
+                '/carpages': {
+                    target: 'http://47.76.135.99:8300',
+                    changeOrigin: true,
+                    ws: true,
+                    rewrite: path => path.replace(/^\/carpages/, '/carpages')
+                },
             },
         },
-    },
-    plugins: [
-        vue(),
-        AutoImport({
-            imports: [
-                'vue',
-                {
-                    'naive-ui': [
-                        'useDialog',
-                        'useMessage',
-                        'useNotification',
-                        'useLoadingBar'
-                    ]
-                }
-            ]
-        }),
-        Components({
-            resolvers: [NaiveUiResolver()]
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
-})
+        plugins: [
+            vue(),
+            AutoImport({
+                imports: [
+                    'vue',
+                    {
+                        'naive-ui': [
+                            'useDialog',
+                            'useMessage',
+                            'useNotification',
+                            'useLoadingBar',
+                        ],
+                    },
+                ],
+            }),
+            Components({
+                resolvers: [NaiveUiResolver()],
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+
+        build: {
+            sourcemap: false, // 生产环境不生成 source map
+            rollupOptions: {
+                // 这里可以添加更多的 Rollup 打包配置
+            },
+            // 更多的构建相关配置...
+        },
+    });
+};
